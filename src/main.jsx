ccd4d74608cd0e568ed93232d5bf320821acfcf4
JMighty <jeanmichel.sonn@gmail.com>
2024-11-16 23:28:15 +0100

    correction du path de l'entry point

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,14 @@ import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 import AuthProvider from "./store/AuthProvider.jsx";
+import { BrowserRouter } from "react-router-dom";
 
 createRoot(document.getElementById("root")).render(
-  <AuthProvider>
-    <StrictMode>
-      <App />
-    </StrictMode>
-  </AuthProvider>
+  <BrowserRouter basename="/x">
+    <AuthProvider>
+      <StrictMode>
+        <App />
+      </StrictMode>
+    </AuthProvider>
+  </BrowserRouter>
 );
